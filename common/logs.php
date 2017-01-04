8f0af26597ab6b3a03f171b0486506575ddc2bbb
bertrandweb <feuille.bertrand@orange.fr>
2017-01-04 12:16:54 +0100

    test2

---
+++ common/logs.php
@@ -0,0 +1,18 @@
+<?php
+class logs
+{
+public $date;
+public $today = date_default_timezone_set('Europe/Paris').date(DATE_RFC2822);
+
+function addlog($errtxt)
+  {
+  $fp = fopen('log.txt','a+');
+  echo $today;
+  $retourligne=$errtxt."\r\n";
+  fputs($fp,$retourligne);
+  fclose($fp);
+  }
+}
+
+$date = new logs();
+$date -> addlog("test");
